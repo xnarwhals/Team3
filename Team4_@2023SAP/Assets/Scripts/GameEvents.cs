d4f19d8cef6dfe26e0a72950e13b0364abbf0568
ZigZogger <ziggycooperhenry@gmail.com>
2023-06-29 15:41:43 -0700

    added event dispatcher & game events

---
+++ Team4_@2023SAP/Assets/Scripts/GameEvents.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public class tempEvent : EvtSystem.Event
+    {
+        public int temp = 0;
+    }
+}
