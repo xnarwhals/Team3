b6c81a3035cdecc1f2bb183cd9811d692743ac57
ZigZogger <ziggycooperhenry@gmail.com>
2023-07-13 12:28:40 -0700

    firerate implemented

--- Team4_@2023SAP/Assets/Scripts/ProjectileShooter.cs
+++ Team4_@2023SAP/Assets/Scripts/ProjectileShooter.cs
@@ -6,24 +6,37 @@ public class ProjectileShooter : MonoBehaviour
 {
     public GameObject projectilePrefab;
 
+    public float fireRate = 0.2f;
+
     Vector3 reticlePos;
+
+    float firetimer;
 
     // Start is called before the first frame update
     void Start()
     {
         EvtSystem.EventDispatcher.AddListener<GameEvents.SendReticlePos>(ReceiveReticlePos);
+
+        firetimer = fireRate;
     }
 
     // Update is called once per frame
     void Update()
     {
+        firetimer += Time.deltaTime;
+
         if (Input.GetMouseButtonDown(1))
         {
-            EvtSystem.EventDispatcher.Raise(new GameEvents.ShootProjectile());
+            if (firetimer >= fireRate)
+            {
+                firetimer = 0.0f;
 
-            GameObject obj = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
-            Projectile p = obj.GetComponent<Projectile>();
-            p.direction = (Vector2)(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+                EvtSystem.EventDispatcher.Raise(new GameEvents.ShootProjectile());
+
+                GameObject obj = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+                Projectile p = obj.GetComponent<Projectile>();
+                p.direction = (Vector2)(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+            }
         }
     }
 
