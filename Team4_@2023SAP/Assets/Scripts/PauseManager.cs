c8ddb51cd3fec5856c1a9ec423d8609383e560cd
ZigZogger <ziggycooperhenry@gmail.com>
2023-07-26 14:19:15 -0700

    pause screen is pretty much done

---
+++ Team4_@2023SAP/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] GameObject mainCanvas;
+    [SerializeField] GameObject controlCanvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button7))
+        {
+            if (!controlCanvas.activeInHierarchy)
+            {
+                if (!mainCanvas.activeInHierarchy)
+                {
+                    mainCanvas.SetActive(true);
+                    Time.timeScale = 0.0f;
+                }
+                else
+                {
+                    ContinueClicked(); //does the same thing as the button
+                }
+            }
+            else
+            {
+                Back();
+            }
+        }
+    }
+
+    public void ContinueClicked()
+    {
+        mainCanvas.SetActive(false);
+        Time.timeScale = 1.0f; //if we do bullet time, change this!
+    }
+
+    public void RestartClicked()
+    {
+        //scene things most likely
+    }
+
+    public void ControlsClicked()
+    {
+        mainCanvas.SetActive(false);
+        controlCanvas.SetActive(true);
+    }
+
+    public void ExitClicked()
+    {
+        //scene things
+    }
+
+    public void Back()
+    {
+        mainCanvas.SetActive(true);
+        controlCanvas.SetActive(false);
+    }
+}
