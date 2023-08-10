9e3ad41e43c636beef32cdb7c1be83f3708ac17e
Gamerlink7 <Rogelio.lara007@gmail.com>
2023-08-10 16:06:48 -0700

    Getting Started on Game Over Scene, Small Changes in Main Menu/End Credits, New Folder for Voice Acting

--- Team4_@2023SAP/Assets/Scripts/Wait.cs
+++ Team4_@2023SAP/Assets/Scripts/Wait.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Wait : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float wait_time = 5f;
     void Start()
     {
-        
+        StartCoroutine(Wait_for_intro());
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator Wait_for_intro()
     {
-        
+        yield return new WaitForSeconds(wait_time);
+
+        SceneManager.LoadScene(1);
     }
 }
