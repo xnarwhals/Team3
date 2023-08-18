9a8a3b9abb426da42a568414b185de70cd350de5
Gamerlink7 <Rogelio.lara007@gmail.com>
2023-08-18 14:21:20 -0700

    Beginning Expo Scene Slightly Working + New Folder for Marketplace Art + Marketplace with correct dialogue, art and word placements

---
+++ Team4_@2023SAP/Assets/Scripts/Beginning Exposition.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // Being able to edit the next mesh pro through the code
+
+public class BeginningExposition : MonoBehaviour
+{
+
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences; // Will hold all the sentences that hold our dialogue
+    private int index;
+    public float typingSpeed;
+    public GameObject continueButton;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+    void Update()
+    {
+        if(textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true); // When the sentence is done, showcase the continue button
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    public void NextSentence()
+    {
+
+        continueButton.SetActive(false);
+
+        if (index > sentences.Length - 1) // Make sure we have not come to the end of our dialogue
+        {
+            index++; // of 1
+            textDisplay.text = ""; // To prevent sentences to stack
+            StartCoroutine(Type()); // A new sentence slowly displays itself
+        }
+        else
+        {
+            textDisplay.text = ""; // Resets the text
+            continueButton.SetActive(false);
+        }
+
+    }
+}
+
