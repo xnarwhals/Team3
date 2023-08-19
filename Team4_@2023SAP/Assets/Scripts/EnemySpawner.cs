2ed16d7a3bb4761cddbc36b65b0c3fd3cd8c47a6
ZigZogger <ziggycooperhenry@gmail.com>
2023-08-18 18:36:14 -0700

    added max enemy count and row selector for spawning

--- Team4_@2023SAP/Assets/Scripts/EnemySpawner.cs
+++ Team4_@2023SAP/Assets/Scripts/EnemySpawner.cs
@@ -8,14 +8,19 @@ public class EnemySpawner : MonoBehaviour
 
     public Vector2 TimeRange = new Vector2(8.0f, 12.0f);
     public Vector2Int EnemyCountRange = new Vector2Int(2, 4);
+    public Vector2Int RowRange = new Vector2Int(0, 4);
+
+    public int MaxEnemyCount = 5;
 
     float currentTime = 0.0f;
     float timer = 0.0f;
 
+    List<GameObject> enemies = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        EvtSystem.EventDispatcher.AddListener<GameEvents.EnemyDie>(RemoveEnemy);
     }
 
     // Update is called once per frame
@@ -29,13 +34,38 @@ public class EnemySpawner : MonoBehaviour
             currentTime = Random.Range(TimeRange.x, TimeRange.y);
 
             int count = Random.Range(EnemyCountRange.x, EnemyCountRange.y);
-            for (int i = 0; i < count; i++)
-            {
-                GameObject enemy = Instantiate(EnemyPrefab);
+            if (count + enemies.Count >= MaxEnemyCount) 
+                count = MaxEnemyCount - enemies.Count;
 
-                EnemySpawn spawn = enemy.GetComponent<EnemySpawn>();
-                spawn.spawnRow = Random.Range(0, GameGrid.Instance.height);
-                spawn.startFromRight = Random.value > 0.5f;
+            SpawnEnemies(count);
+        }
+    }
+
+    void SpawnEnemies(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            GameObject enemy = Instantiate(EnemyPrefab);
+            RegisterEnemy(enemy);
+
+            EnemySpawn spawn = enemy.GetComponent<EnemySpawn>();
+            spawn.spawnRow = Random.Range(RowRange.x, RowRange.y);
+            spawn.startFromRight = Random.value > 0.5f;
+        }
+    }
+
+    void RegisterEnemy(GameObject enemy)
+    {
+        enemies.Add(enemy);
+    }
+
+    void RemoveEnemy(GameEvents.EnemyDie evt)
+    {
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (evt.enemy == enemies[i])
+            {
+                enemies.Remove(enemies[i]);
             }
         }
     }
