37a82508ea6e46938a022d3d56d6ffb28877661d
ZigZogger <ziggycooperhenry@gmail.com>
2023-08-26 01:24:05 -0700

    activated pause menu

--- Team4_@2023SAP/Assets/Scripts/PaintShooter.cs
+++ Team4_@2023SAP/Assets/Scripts/PaintShooter.cs
@@ -40,6 +40,7 @@ public class PaintShooter : Singleton<PaintShooter>
         if ((Input.GetMouseButton(0) || Input.GetKey(KeyCode.JoystickButton0)) 
             && fireTimer >= fireRate)
         {
+            print("wha");
             PaintExample paintScript = FindAnyObjectByType<PaintExample>(); //fixes weird bug
             if (paintScript.CanPaint())
             {
